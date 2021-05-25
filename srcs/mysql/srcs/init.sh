1020c3a2d7fab06178502ef43a5fc16b9a754cdc
kefujiwa <kefujiwa@student.42tokyo.jp>
2021-05-25 18:29:39 +0900

    feat(mysql): implement Dockerfile, init.sh, and configuration files for mysql

---
+++ srcs/mysql/srcs/init.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+if [ ! -d /var/lib/mysql/$MYSQL_DATABASE ]; then
+# initialize MariaDB
+	mysql_install_db --user=mysql --datadir=/var/lib/mysql --auth-root-authentication-method=normal
+	cat << EOF > init.sql
+USE mysql;
+FLUSH PRIVILEGES ;
+SET PASSWORD FOR 'root'@'localhost'=PASSWORD('${MYSQL_ROOT_PASSWORD}') ;
+GRANT ALL ON *.* TO 'root'@'%' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}' ;
+CREATE DATABASE ${MYSQL_DATABASE} CHARACTER SET utf8;
+CREATE USER '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}' ;
+GRANT ALL ON ${MYSQL_DATABASE}.* TO ${MYSQL_USER}@'%' IDENTIFIED BY '${MYSQL_PASSWORD}' ;
+FLUSH PRIVILEGES ;
+EOF
+	/usr/bin/mysqld --user=mysql --bootstrap < init.sql
+	rm -f init.sql
+fi
+
+chown -R mysql:mysql /var/lib/mysql/
+
+telegraf &
+
+/usr/bin/mysqld_safe --datadir=/var/lib/mysql --plugin-dir=/usr/lib/mariadb/plugin
+
+tail -f /var/lib/mysql/mysql-*.err
